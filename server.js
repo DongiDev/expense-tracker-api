6c944090a0c35e2b4aea6420da10413743061386
DongiDev <dongihoxate202850@gmail.com>
2025-12-11 14:16:13 +0700

    Add redirect to login page

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.use('/api/auth', authRoutes);
 const transactionRoutes = require('./routes/transactionRoutes');
 app.use('/api/transactions', transactionRoutes);
 
+// ถ้าใครเข้ามาหน้าแรก (/) ให้ส่งไปหน้า login ทันที
+app.get('/', (req, res) => {
+    res.redirect('/login.html');
+});
+
 
 //! เปิด Server
 const PORT = 3000;
